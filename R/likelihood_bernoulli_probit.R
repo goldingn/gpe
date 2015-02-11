cff4544210231e07206904cd31e10bb158f3b67c
Nick <goldingn35@gmail.com>
2015-02-11 22:01:41 +0000

    added bernoulli_probit log-likelihood function and first two derivatives

---
+++ R/likelihood_bernoulli_probit.R
@@ -0,0 +1,72 @@
+# likelihood_bernoulli
+
+bernoulli_probit <- function(y, f, which = c('d0', 'd1', 'd2')) {
+  # bernoulli log-likelihood (and its derivatives)
+  # with the probit link function
+  # y is the observed data, either binary (0, 1) or proportion
+  # f is the value of the correspnding latent gaussians
+  # which determines whether to return the log-likelihood (d0)
+  # or it's first or second derivative
+
+  # check which derivative is needed
+  which <- match.arg(which)
+  
+  # repeat y if needed
+  if(length(y) != length(f)) y <- rep(y, length(f))
+  
+  # find observations which are actually probabilities
+  # (can be used to handle proportion data with unknown sample size)
+
+  # proportions
+  pr <- y > 0 & y < 1
+  
+  # not proportions
+  npr <- !pr
+
+  # switch true binary data from 0, 1 to -1, 1
+  y[npr] <- 2 * y[npr] - 1
+  
+  # create an empty vector for the results
+  ans <- vector('numeric', length(y))
+  
+  # straight likelihood case
+  if (which == 'd0') {
+  
+    # for proportion data, convert to univariate Gaussian
+    # and get log-density
+    y[pr] <- qnorm(y[pr])
+    ans[pr] <- dnorm(y[pr],
+                     f[pr],
+                     log = TRUE)
+    
+    # for true binary data, get cumulative density of the unit gaussian
+    # evaluated at f
+    ans[npr] <- pnorm(y[npr] * f[npr],
+                      log.p = TRUE)
+  
+  } else if (which == 'd1') {
+    # first derivative
+    
+    # for proportion data, convert to univariate Gaussian and evaluate
+    y[pr] <- qnorm(y[pr])
+    ans[pr] <- y[pr] - f[pr]
+    
+    # for binary data...
+    ans[npr] <- y[npr] * dnorm(f[npr]) / pnorm(y[npr] * f[npr])
+    
+  } else {
+    # second derivative
+    
+    # all proportions get -1, apparently
+    ans[pr] <- -1
+    
+    # the binary data gets something more complex
+    a <- dnorm(f[npr]) ^ 2 / pnorm(y[npr] * f[npr]) ^ 2
+    b <- y[npr] * f[npr] * dnorm(f[npr]) / pnorm(y[npr] * f[npr])
+    ans[npr] <- -a - b
+
+  }
+  
+  return (ans)
+
+}
