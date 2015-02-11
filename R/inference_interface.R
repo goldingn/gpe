2fe3373b404cf9246c1b5947f68c1cde63a35195
Nick <goldingn35@gmail.com>
2015-02-11 22:02:13 +0000

    added skeleton of an interface for inference methods

---
+++ R/inference_interface.R
@@ -0,0 +1,68 @@
+gp <- function(response,
+               kernel,
+               data,
+               family = gaussian,
+               mean_function = NULL,
+               inducing_points = NULL,
+               inference = c('default', 'exact', 'FITC')) {
+
+  # capture family as a string
+  family <- deparse(substitute(family))
+  
+  # and inference
+  inference <- match.arg(inference)
+  # check it's a valid kernel
+  checkKernel(kernel)  
+  
+  # get inference function
+  inference <- getInference(inference, family)
+
+  
+}
+
+checkInference(inference, family) {
+  
+  if (inference %in% c('exact', 'FITC') &
+        family != 'gaussian') {
+    stop ('exact and FITC inference are only possible with the gaussian family')
+  }
+  
+}
+
+defaultInference <- function (family) {
+  
+  inference <- switch(family,
+                      gaussian = 'exact')
+  
+  # if nothing found, throw an error
+  if (is.null(inference)) {
+    stop (paste0('no default inference method found for family: ',
+                 family))
+  }
+  
+  return (inference)
+  
+}
+
+getInference <- function (inference, family) {
+
+  # if it's default, look up the default
+  if (inference == 'default') {
+    inference <- defaultInference(family)
+  }
+  
+  # check inference method is suitable for the family
+  checkInference(inference, family)
+  
+  # switch inference to method
+  inference <- switch(inference,
+                      exact = infExact,
+                      FITC = infFITC)
+  
+  if (is.null(inference)) {
+    stop ('inference method not available')
+  }
+  
+  return (inference)
+
+}
