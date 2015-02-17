c092179ef272c8e4f040cf8c7203ac4c67a54c34
Nick <goldingn35@gmail.com>
2015-02-17 20:09:57 +0000

    added poisson likelihood - still need to double-check derivatives

---
+++ R/likelihood_poisson_log.R
@@ -0,0 +1,44 @@
+# likelihood_poisson_log
+
+poisson_log <- function(y, f, which = c('d0', 'd1', 'd2'), ...) {
+  # poisson log-likelihood (and its derivatives)
+  # with the log link function
+  # y is the observed data, must be counts
+  # f is the value of the corresponding latent gaussians
+  # which determines whether to return the log-likelihood (d0)
+  # or it's first or second derivative
+  # \dots doesn't do anything, but is there for compatibility with other
+  # likelihood which might use it
+  
+  # check which derivative is needed
+  which <- match.arg(which)
+  
+  # repeat y if needed
+  if(length(y) != length(f)) y <- rep(y, length(f))
+  
+  # create an empty vector for the results
+  ans <- vector('numeric', length(y))
+  
+  # straight likelihood case
+  if (which == 'd0') {
+
+    # get log-density
+    ans <- dpois(y,
+                 exp(f),
+                 log = TRUE)
+    
+  } else if (which == 'd1') {
+    
+    # first derivative
+    ans <- y / exp(f) - 1
+    
+  } else {
+
+    # second derivative
+    ans <- -y / (exp(f) ^ 2)
+    
+  }
+  
+  return (ans)
+  
+}
