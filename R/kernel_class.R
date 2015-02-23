b8aa2e5fcf43ca1e2b1371f7271489ed89cb37bd
Nick <goldingn35@gmail.com>
2015-02-23 10:57:29 +0000

    added kernel plotting function

--- R/kernel_class.R
+++ R/kernel_class.R
@@ -8,6 +8,13 @@
 #' @description Generic functions associated with the kernel class.
 #' @template kcl_kernel
 #' 
+#' @details \code{is.kernel} returns a logical indicating whether the 
+#' object is a gpe kernel. \code{print} returns a very simple summary of the 
+#' kernel structure. \code{summary} returns a more detailed summary of the 
+#' kernel structure, including the values of the kernel parameters.
+#' \code{plot} plots the covariance structure of the kernel (how covariance)
+#' between two points depends on the distance between them.
+#' 
 #' @examples
 #'  
 #' # construct a kernel with one feature
@@ -37,7 +44,8 @@ as.kernel <- function (x) {
 is.kernel <- function (x) {
   
   # test whether x is a kernel function
-  ans <- inherits(x, "kernel")
+  ans <- inherits(x, "kernel") &
+    inherits(x, "function")
   
   # return the answer
   return (ans)
@@ -122,4 +130,100 @@ summary.kernel <- function (object, ..., digits = max(3, getOption("digits")-3))
     dumpDetails(kernel_data[[2]][[1]], digits)
   }
   
+}
+
+
+
+#' @rdname kernel
+#' @param which a string giving the column name for which to plot the 
+#' covariance structure. If \code{NULL} a plot will be produced for each 
+#' caolumn on which the kernel acts
+#' @param range the range of distances between points over which to plot 
+#' covariances
+#' @param ref the reference value against which to compare other points 
+#' of a given distance
+#' 
+#' @export
+#' @examples
+#'  
+#' # plot the covariance structure of the GP
+#' plot(k1)
+#' 
+plot.kernel <- function(x, which = NULL, range = c(-5, 5), ref = 0, ...) {
+  
+  # check x is a kernel object
+  checkKernel(x)
+  
+  # check ref and range are sensible
+  if (! ((ref <= range[2] & ref >= range[1]) |
+           (ref >= range[2] & ref <= range[1]))) {
+    stop ('ref must fall between the two values of range')
+  }
+  
+  stopifnot(is.finite(ref))
+  stopifnot(all(is.finite(range)))
+  # get the columns on which the kernel acts
+  columns <- getColumns(x)
+  
+  # number of columns
+  D <- length(columns)
+  
+  if (is.null(which)) {
+    # if they want all columns plotted, recursively call this function
+    
+    for (column in columns) {
+      plot(x,
+           which = column,
+           range = range,
+           ref = ref,
+           ...)
+    }
+    
+    
+    
+  } else {
+    
+    # otherwise act only on that column
+    
+    # check it's a valid column
+    if (!(which %in% columns)) {
+      stop (paste0('the kernel does not act on column ',
+                   which,
+                   ', it only acts on columns: ',
+                   paste(columns, collapse = ', ')))
+    }
+    
+    # number of points in the line to predict to
+    n_predict <- 500
+    
+    # create dummy dataframes containing 0s for all variables
+    df_from <- data.frame(replicate(D,
+                                    0,
+                                    simplify = FALSE))
+    
+    df_to <- data.frame(replicate(D,
+                                  rep(0, n_predict),
+                                  simplify = FALSE))
+    
+    # give them the correct names
+    names(df_from) <- names(df_to) <- columns
+    
+    # assign to the column of interest
+    df_from[, which] <- ref
+    df_to[, which] <- seq(range[1], range[2], len = 500)
+    
+    # evaluate the kernel on these dataframes
+    K <- x(df_from, df_to)
+    
+    plot(K[1, ] ~ df_to[, which],
+         type = 'l',
+         lwd = 3,
+         col = grey(0.3),
+         ylab = 'K_ij',
+         xlab = paste0(which,
+                       '_i - ',
+                       which,
+                       '_j'),
+         ...)
+  }
 }
