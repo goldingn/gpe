c748b25ac951ffb4ba140a1eb1ffb8d3af91d8bc
Nick Golding <goldingn35@gmail.com>
2016-06-30 15:50:41 -0700

    adding more testing of GP object methods

--- tests/testthat/test_fitting.R
+++ tests/testthat/test_fitting.R
@@ -31,6 +31,69 @@ test_that('model fitting options work', {
                                 data = df,
                                 verbose = TRUE))
   
+  # offsets
+  m_gaus <- gp(y_gaus ~ rbf('x') + iid('z') + iid() + offset(x),
+               data = df)
+  
+  # ~~~~~~~~~~~
+  # weights
+  m_gaus <- gp(y_pois ~ rbf('x') + iid('z') + iid(),
+               data = df,
+               family = poisson,
+               weights = runif(n))
+
+  # wring length
+  expect_error(m_gaus <- gp(y_pois ~ rbf('x') + iid('z') + iid(),
+                            data = df,
+                            family = poisson,
+                            weights = runif(3)))
+  
+  # non-numeric
+  expect_error(m_gaus <- gp(y_pois ~ rbf('x') + iid('z') + iid(),
+                            data = df,
+                            family = poisson,
+                            weights = sample(letters, n, replace = TRUE)))
+
+  # non-vector
+  expect_error(m_gaus <- gp(y_pois ~ rbf('x') + iid('z') + iid(),
+                            data = df,
+                            family = poisson,
+                            weights = as.list(runif(n))))
+
+  # non finite
+  expect_error(m_gaus <- gp(y_pois ~ rbf('x') + iid('z') + iid(),
+                            data = df,
+                            family = poisson,
+                            weights = rep(NA, n)))
+
+  # negative
+  expect_error(m_gaus <- gp(y_pois ~ rbf('x') + iid('z') + iid(),
+                            data = df,
+                            family = poisson,
+                            weights = runif(n, -1, 0)))
+  
+  # ~~~~~~~~~~~
+  # mean functions
+  
+
+  # not a function
+  expect_error(m_gaus <- gp(y_pois ~ rbf('x') + iid('z') + iid(),
+                            data = df,
+                            family = poisson,
+                            mean_function = list()))
+  
+  # bad function
+  expect_error(m_gaus <- gp(y_pois ~ rbf('x') + iid('z') + iid(),
+                            data = df,
+                            family = poisson,
+                            mean_function = function (data) {2 + -3 * data$tomatoes}))
+
+  # bad function
+  expect_error(m_gaus <- gp(y_pois ~ rbf('x') + iid('z') + iid(),
+                            data = df,
+                            family = poisson,
+                            mean_function = function (data) {2 + -3 * data$x[1:3]}))
+  
   # ~~~~~~
   # check sparse models
   
@@ -44,10 +107,10 @@ test_that('model fitting options work', {
                data = df,
                inducing_data = df[1:50, ],
                inference = 'FITC')
-
+  
   # ~~~~~
   # check latent inference methods
-
+  
   # currently no Gaussian+Laplace
   expect_error(m_gaus <- gp(y_gaus ~ rbf('x') + iid('z') + iid(),
                             data = df,
@@ -75,7 +138,7 @@ test_that('model fitting options work', {
   m_gaus <- gp(y_gaus ~ rbf('x') + iid(),
                data = df,
                hyperinference = 'BFGS')
-
+  
   m_gaus <- gp(y_gaus ~ rbf('x') + iid(),
                data = df,
                hyperinference = 'BFGSrestarts')
@@ -83,5 +146,62 @@ test_that('model fitting options work', {
   m_gaus <- gp(y_gaus ~ rbf('x') + iid('z') + iid(),
                data = df,
                hyperinference = 'none')
+  
+})
+
+test_that('model prediction works', {
+  
+  # create some models
+  m_gaus <- gp(y_gaus ~ rbf('x') + iid('z') + iid(),
+               data = df)
+  m_pois <- gp(y_pois ~ rbf('x') + iid('z'),
+               data = df,
+               family = poisson)
+  
+  # ~~~~~~~~~
+  
+  # predict to new df
+  pred_gaus_new <- predict(m_gaus, df[1:50, ])
+  pred_pois_new <- predict(m_pois, df[1:50, ])
+  
+  # self predict
+  pred_gaus <- predict(m_gaus)
+  pred_pois <- predict(m_pois)
+  pred_pois_resp <- predict(m_pois, type = 'response')
+  pred_pois_resp_sd <- predict(m_pois, type = 'response', sd = TRUE)
+  
+  # check classes
+  expect_equal(class(pred_gaus), 'matrix')
+  expect_equal(class(pred_pois), 'matrix')
+  expect_equal(class(pred_gaus_new), 'matrix')
+  expect_equal(class(pred_pois_new), 'matrix')
+  
+  # response predictions apparently aren't matrices
+  # expect_equal(class(pred_pois_resp), 'matrix')
+  
+  expect_equal(class(pred_pois_resp_sd), 'list')
+  
+  # check dimensions
+  expect_equal(dim(pred_gaus), c(100, 1))
+  expect_equal(dim(pred_pois), c(100, 1))
+  expect_equal(dim(pred_gaus_new), c(50, 1))
+  expect_equal(dim(pred_pois_new), c(50, 1))
+  
+})
+
+test_that('model printing works', {
+  
+  # create some models
+  m_gaus <- gp(y_gaus ~ rbf('x') + iid('z') + iid(),
+               data = df)
+  m_pois <- gp(y_pois ~ rbf('x') + iid('z'),
+               data = df,
+               family = poisson)
+
+  expected <- c("A Gaussian process model fitted against 100 observations",
+                "\tcall: \t\t\t gp(formula = y_gaus ~ rbf(\"x\") + iid(\"z\") + iid(), data = df) ",
+                "\tkernel: \t\t(rbf(x) + iid(z)) + iid() ")  
+  expect_equal(capture.output(print(m_gaus)),
+               expected)
   
 })
